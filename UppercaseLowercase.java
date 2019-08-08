07ae91e1e525989eaebad04ef595ab767c629bf3
swami3690 <53114582+swami3690@users.noreply.github.com>
2019-08-08 22:25:43 +0100

    Add files via upload

---
+++ UppercaseLowercase.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UppercaseLowercase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the alphabet");
+String u = scanner.next();
+System.out.println(u.toLowerCase());
+
+
+
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
